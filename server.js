1bba5642c3f59f2ca17b12226d00d00a7ea31353
Ann <ann.thuhien@gmail.com>
2021-07-15 21:10:48 -0400

    update again

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const corsProxy = require("cors-anywhere");
 
-// Listen on a specific host via the HOST environment variable.
 const host = process.env.HOST || "0.0.0.0";
-// Listen on a specific port via the PORT environment variable.
 const port = process.env.PORT || 8080;
 
 corsProxy
